f0e3f455c0984d0f1df9b9241dbcf02b221e9c5f
LavanyaThangapandiyan <128019444+LavanyaThangapandiyan@users.noreply.github.com>
2023-03-25 22:21:21 +0530

    Create Login.java

---
+++ src/com/exceptionhandling/Login.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Login
+{
+	private String email;
+	private String password;
+	String login(String email,String password) throws Exception 
+	{
+			if((email.equals("validuser@gmail.com"))&& (password.length()<=6)) 
+			{
+				return ("Verified");
+			}
+			else
+			{
+				throw new Exception("invalid email and password");
+			}
+	}
+}
+class LoginForm 
+{  
+    public static void main(String arg[]) throws Exception  
+    {  
+    	String email,password;
+    	String ex;
+    	Scanner sc= new Scanner(System.in);
+    	System.out.println("enter the email id");
+    	email=sc.nextLine();
+    	System.out.println("enter the password");
+    	password=sc.nextLine();
+    	Login obj=new Login();
+    	ex=obj.login(email,password);
+        try  
+        {  
+        	System.out.println(ex);
+        }  
+        catch(Exception e)  
+        {     
+        	System.out.println(e);
+        }  
+    }  
+}
